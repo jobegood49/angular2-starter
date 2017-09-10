3cae8fa71a7e916af7140fd3193e605edd4ee443
jonathannicolas <jonathan.s.nicolas@gmail.com>
2017-09-10 23:41:47 +0200

    Amend package.json

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by jonathannicolas on 09/09/2017.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app/app.component';
+
+bootstrap(appComponent);
+
